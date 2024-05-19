35fba4a100774bbab2d946c4d5544b262ee3e0ec
Francisco Josafá <josafa.lopes@inventione.com.br>
2024-05-19 19:33:31 -0300

    Add project files.

---
+++ Models/CategoriaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CategoriasMvc.Models
+{
+    public class CategoriaViewModel
+    {
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "O nome da cateria é obrigatório")]
+        public string? Nome { get; set; }
+        
+        [Display(Name = "Imagem")]
+        public string? ImagemUrl { get; set; }
+    }
+}
