5ce76310ec6354234e626b8020fc2316fa0afbb1
Francisco Josafá <josafa.lopes@inventione.com.br>
2024-05-19 22:53:22 -0300

    Implementa o tratamentoglobal de exceções

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,27 @@
+﻿using CategoriasMvc.Models;
+using CategoriasMvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriasMvc.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriasController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        public async Task<ActionResult<IEnumerable<CategoriaViewModel>>> Index()
+        {
+            var result = await _categoriaService.GetCategorias();
+
+            if (result == null)
+            {
+                return View("Error");
+            }
+            return View(result);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using CategoriasMvc.Extensions;
 using CategoriasMvc.Services;
 using System.Net.Http.Headers;
 
@@ -24,7 +25,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.ConfigureExceptionHandler();
+    //app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
